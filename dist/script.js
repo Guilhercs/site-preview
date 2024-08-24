7ea5d414a565d4abeec4f496adf3b1f6379bda00
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2024-08-23 22:05:38 -0300

    chore: initial commit

--- dist/script.js
+++ dist/script.js
@@ -1,73 +1,19 @@
-$(function () {
- $(".menu-link").click(function () {
-  $(".menu-link").removeClass("is-active");
-  $(this).addClass("is-active");
- });
-});
+document.addEventListener("DOMContentLoaded", function () {
+  const smoothScrollLinks = document.querySelectorAll('a[href^="#"]');
 
-$(function () {
- $(".main-header-link").click(function () {
-  $(".main-header-link").removeClass("is-active");
-  $(this).addClass("is-active");
- });
-});
+  smoothScrollLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
 
-const dropdowns = document.querySelectorAll(".dropdown");
-dropdowns.forEach((dropdown) => {
- dropdown.addEventListener("click", (e) => {
-  e.stopPropagation();
-  dropdowns.forEach((c) => c.classList.remove("is-active"));
-  dropdown.classList.add("is-active");
- });
-});
+      const targetId = this.getAttribute("href");
+      const targetElement = document.querySelector(targetId);
 
-$(".search-bar input")
- .focus(function () {
-  $(".header").addClass("wide");
- })
- .blur(function () {
-  $(".header").removeClass("wide");
- });
-
-$(document).click(function (e) {
- var container = $(".status-button");
- var dd = $(".dropdown");
- if (!container.is(e.target) && container.has(e.target).length === 0) {
-  dd.removeClass("is-active");
- }
-});
-
-$(function () {
- $(".dropdown").on("click", function (e) {
-  $(".content-wrapper").addClass("overlay");
-  e.stopPropagation();
- });
- $(document).on("click", function (e) {
-  if ($(e.target).is(".dropdown") === false) {
-   $(".content-wrapper").removeClass("overlay");
-  }
- });
-});
-
-$(function () {
- $(".status-button:not(.open)").on("click", function (e) {
-  $(".overlay-app").addClass("is-active");
- });
- $(".pop-up .close").click(function () {
-  $(".overlay-app").removeClass("is-active");
- });
-});
-
-$(".status-button:not(.open)").click(function () {
- $(".pop-up").addClass("visible");
-});
-
-$(".pop-up .close").click(function () {
- $(".pop-up").removeClass("visible");
-});
-
-const toggleButton = document.querySelector('.dark-light');
-
-toggleButton.addEventListener('click', () => {
-  document.body.classList.toggle('light-mode');
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
 });
